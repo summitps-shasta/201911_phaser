c497ca82aa7b8bb65cefbc70e58b0ae22480455b
EC2 Default User <ec2-user@ip-172-31-38-55.us-east-2.compute.internal>
2019-11-21 22:06:14 +0000

    added sprite

--- myGame2/myGame2.js
+++ myGame2/myGame2.js
@@ -9,15 +9,23 @@ game_state.main.prototype = {
     preload: function() {
         game.load.image('player', 'assets/player.png');
         game.load.image('object', 'assets/object.png');
+        game.load.spritesheet('that', 'assets/that.png', 320, 320);
+
     },
+
+
 
     create: function() {
         game.stage.backgroundColor = '#3598db';
         game.physics.startSystem(Phaser.Physics.ARCADE);
-        this.player = game.add.sprite(200, 400, 'player');
+        this.player = game.add.sprite(200, 300, 'that');
 
         game.physics.arcade.enable(this.player);
         this.player.enableBody = true;
+        
+        this.player.animations.add('move', [0], 10, true);
+        this.player.body.setSize(300, 300, 10, 10);
+        
         this.player.body.immovable = true;
 
         this.left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
@@ -36,12 +44,16 @@ game_state.main.prototype = {
     update: function() {
         if (this.left.isDown) {
             this.player.body.velocity.x = -300;
+            this.player.animations.play('move');
         }
         else if (this.right.isDown) {
             this.player.body.velocity.x = 300;
+            this.player.animations.play('move');
         }
         else {
             this.player.body.velocity.x = 0;
+            this.player.animations.stop();
+            this.player.frame = 1;
         }
         game.physics.arcade.overlap(this.player, this.objects, this.hitObject, null, this);
 
