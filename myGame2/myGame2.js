fb64c013205e476f2492d2b8551c81baa2e7683a
EC2 Default User <ec2-user@ip-172-31-31-87.us-east-2.compute.internal>
2019-11-21 21:34:14 +0000

    Added sprites

--- myGame2/myGame2.js
+++ myGame2/myGame2.js
@@ -9,6 +9,8 @@ game_state.main.prototype = {
     preload: function() {
         game.load.image('player', 'assets/player.png');
         game.load.image('object', 'assets/object.png');
+        
+        game.load.spritesheet('person', 'assets/person.png', 144, 144);
 
 
     },
@@ -16,13 +18,17 @@ game_state.main.prototype = {
     create: function() {
         game.stage.backgroundColor = '3598db';
         game.physics.startSystem(Phaser.Physics.ARCADE);
-        this.player = game.add.sprite(200, 400, 'player');
+        this.player = game.add.sprite(200, 400, 'person');
         game.physics.arcade.enable(this.player);
         this.player.enableBody = true;
         this.player.body.immovable = true;
 
         this.left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
         this.right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+
+
+        this.player.animations.add('left', [2], 10, true);
+        this.player.animations.add('right', [1], 10, true);
 
         this.objects = game.add.group();
         this.objects.enableBody = true;
@@ -44,13 +50,17 @@ game_state.main.prototype = {
     update: function() {
         if (this.left.isDown) {
             this.player.body.velocity.x = -300;
+            this.player.animations.play('left');
         }
         else if (this.right.isDown) {
             this.player.body.velocity.x = 300;
+             this.player.animations.play('right');
         }
 
         else {
             this.player.body.velocity.x = 0;
+            this.player.animations.stop();
+            this.player.frame=0;
         }
 
         game.physics.arcade.overlap(this.player, this.objects, this.hitObject, null, this);
