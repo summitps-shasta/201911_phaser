fd92fad00e40776c51d2cea56c59c74473e67dea
EC2 Default User <ec2-user@ip-172-31-37-233.us-east-2.compute.internal>
2019-12-02 01:10:27 +0000

    base tutorial + animated player image

--- myGame2/myGame2.js
+++ myGame2/myGame2.js
@@ -1,20 +1,23 @@
-/*global Phaser*/
-
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '');
-var game_state = {}
+/*  global game, game_state, Phaser  */
 
 game_state.main = function() {};
+
 game_state.main.prototype = {
 
     preload: function() {
-        game.load.image('player', 'assets/player.png');
+//      game.load.image('player', 'assets/player.png');
         game.load.image('object', 'assets/object.png');
+        game.load.spritesheet('player', 'assets/nar 1.png', 128, 128, 5);
     },
 
     create: function() {
         game.stage.backgroundColor = '#3598db';
 
-        this.player = game.add.sprite(200, 400, 'player')
+        this.player = game.add.sprite(200, 400, 'player');
+        this.player.animations.add('right', [1, 3], 10, true);
+        this.player.animations.add('left',  [2, 4], 10, true);
+//      this.player.scale.setTo(4, 4);
+
         game.physics.arcade.enable(this.player);
         this.player.enableBody = true;
         this.player.body.immovable = true;
@@ -31,17 +34,24 @@ game_state.main.prototype = {
               _this.objects.create(
               Math.random() * 800, -64, 'object');
             object.body.gravity.y = 300;
-        }, 1000)
+        }, 1000);
         
     },
 
     update: function() {
-        if (this.left.isDown)
+        if (this.left.isDown) {
             this.player.body.velocity.x = -300;
-        else if (this.right.isDown)
+            this.player.animations.play('left');
+        }
+        else if (this.right.isDown) {
             this.player.body.velocity.x = 300;
-        else
+            this.player.animations.play('right');
+        }
+        else {
             this.player.body.velocity.x = 0;
+            this.player.animations.stop();
+            this.player.frame = 0;
+        }
 
         game.physics.arcade.overlap(this.player, this.objects, this.hitObject, null, this);
     },
@@ -50,6 +60,6 @@ game_state.main.prototype = {
         object.kill();
     }
     
-}
+};
+
 game.state.add('main', game_state.main);
-game.state.start('main');
