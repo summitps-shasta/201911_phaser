a5a97929d79866702fb0ef5eed78d4645329e309
EC2 Default User <ec2-user@ip-172-31-37-233.us-east-2.compute.internal>
2019-12-02 22:45:55 +0000

    added platform

--- myGame2/myGame2.js
+++ myGame2/myGame2.js
@@ -8,34 +8,50 @@ game_state.main.prototype = {
 //      game.load.image('player', 'assets/player.png');
         game.load.image('object', 'assets/object.png');
         game.load.spritesheet('player', 'assets/nar 1.png', 128, 128, 5);
+
+        game.load.image('sky',    'assets/sky.png');       //  SKY  preload
+        game.load.image('ground', 'assets/platform.png');  //  PLAT preload
+        game.load.image('star',   'assets/star.png');      //  STAR preload
     },
 
     create: function() {
-        game.stage.backgroundColor = '#3598db';
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+
+//      game.stage.backgroundColor = '#3598db';
+        game.add.sprite(0, 0, 'sky');   //  SKY  create
+        game.add.sprite(0, 0, 'star');  //  STAR create
 
         this.player = game.add.sprite(200, 400, 'player');
         this.player.animations.add('right', [1, 3], 10, true);
         this.player.animations.add('left',  [2, 4], 10, true);
 //      this.player.scale.setTo(4, 4);
-
         game.physics.arcade.enable(this.player);
         this.player.enableBody = true;
         this.player.body.immovable = true;
 
         this.left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
         this.right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+
         this.objects = game.add.group();
         this.objects.enableBody = true;
-        
         var _this = this;
-
         setInterval(function() {
             var object =
               _this.objects.create(
               Math.random() * 800, -64, 'object');
             object.body.gravity.y = 300;
         }, 1000);
-        
+
+    	this.platforms = game.add.group();  //  PLAT create
+    	this.platforms.enableBody = true;   //  PLAT create
+    	var ground = this.platforms.create(0, game.world.height - 64, 'ground');  //  PLAT create
+    	ground.scale.setTo(2, 2);  //  PLAT create
+    	ground.body.immovable = true;  //  PLAT create
+
+    	var ledge1 = this.platforms.create(100, 200, 'ground');  //  PLAT create
+    	ledge1.body.immovable = true;  //  PLAT create
+    	var ledge2 = this.platforms.create(300, 300, 'ground');  //  PLAT create
+    	ledge2.body.immovable = true;  //  PLAT create
     },
 
     update: function() {
