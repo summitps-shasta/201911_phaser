7725f429be076d7d3aa7afc101534270d5d7fb8a
EC2 Default User <ec2-user@ip-172-31-19-243.us-east-2.compute.internal>
2019-12-19 03:47:26 +0000

    added story state

---
+++ myGame2/story.js
@@ -0,0 +1,28 @@
+/*global Phaser game game_state*/
+var game_state = {}
+
+game_state.story = function() {};
+game_state.story.prototype = {
+
+    preload: function() {
+        game.load.image('intro', 'assets/New Piskel (10).png');
+
+    },
+
+    create: function() {
+        game.add.sprite(0, 0, 'intro');
+
+        game.input.keyboard.onPressCallback = function(s) {
+                game.state.start('main');
+        };
+
+    },
+
+    update: function() {
+        
+    },
+};
+game.state.add('story', game_state.story);
+game.state.start('story');
+
+// game.state.start('main');
