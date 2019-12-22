f8716f7607278d0b6f90deb5c37d66105a2016fc
EC2 Default User <ec2-user@ip-172-31-41-169.us-east-2.compute.internal>
2019-12-22 05:09:31 +0000

    a few changes to get start screen - Mr Rick Armstrong Shaul

--- myGame2/myGame2.js
+++ myGame2/myGame2.js
@@ -1,66 +1,122 @@
-/*global Phaser*/
-
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '');
-var game_state = {};
+/*global Phaser game game_state  */
 
 game_state.main = function() {};
 game_state.main.prototype = {
 
     preload: function() {
-//      game.load.spritesheet('player', 'assets/egg.png', 128, 128);
-        game.load.spritesheet('player', 'assets/egg.png', 128, 128);
-        game.load.image('object', 'assets/object.png');
+
+        game.load.image('sky', 'assets/sky.png');
+        game.load.image('ground', 'assets/platform.png');
+        game.load.image('star', 'assets/star.png');
+        game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+
     },
 
     create: function() {
-        // Set the background color to blue
-        game.stage.backgroundColor = '#3598db';
-        // Start the arcade physics system (for movements and collisions)
+
+        this.score = 0;
+
         game.physics.startSystem(Phaser.Physics.ARCADE);
-        // Add the player at the bottom of the screen
-        this.player = game.add.sprite(200, 400, 'player');
-        // We need to enable physics on this.player
+
+        game.add.sprite(0, 0, 'star');
+        game.add.sprite(0, 0, 'sky');
+
+        this.platforms = game.add.group();
+
+        this.platforms.enableBody = true;
+
+        var ground = this.platforms.create(0, game.world.height - 64, 'ground');
+
+        ground.scale.setTo(2, 2);
+
+        ground.body.immovable = true;
+
+        var ledge = this.platforms.create(168, 400, 'ground');
+        
+        var ledge1 = this.platforms.create(100, 200, 'ground');
+        
+        var ledge2 = this.platforms.create(50, 300, 'ground');
+        
+        ledge1.body.immovable = true;
+        
+        ledge2.body.immovable = true;
+        ledge.body.immovable = true;
+
+
+        this.player = game.add.sprite(32, game.world.height - 150, 'dude');
+
         game.physics.arcade.enable(this.player);
-        // Enable body on player
-        this.player.enableBody = true;
-        // Make sure the player won't move when it hits the ball
-        this.player.body.immovable = true;
-        // Create the left/right arrow keys
-        this.left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
-        this.right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
-        // Create objects group
-        this.objects = game.add.group();
-        // Enable body for all objects in the group
-        this.objects.enableBody = true;
-        // Anchor this object to _this variable 
-        var _this = this;
-        // Create objects over time
-        setInterval(function() {
-            // create an object at the top of the screen at a random x
-            var object = _this.objects.create(Math.random() * 800, -64, 'object');
-            // Let gravity do its thing
-            object.body.gravity.y = 300;
-        }, 1000); // 1000 = 1000ms = 1 second
+
+        this.player.body.bounce.y = 0.5;
+        this.player.body.gravity.y = 250;
+        this.player.body.collideWorldBounds.y = true;
+
+        this.player.animations.add('left', [0, 1, 2, 3], 10, true);
+        this.player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+        this.cursors = game.input.keyboard.createCursorKeys();
+
+        this.stars = game.add.group();
+
+        this.stars.enableBody = true;
+
+        for (var i = 0; i < 60; i++) {
+            var star = this.stars.create(i * 70, 0, 'star');
+
+            star.body.gravity.y = 300;
+
+            star.body.bounce.y = 0.7 + Math.random() * 0.2;
+            this.scoreText = game.add.text(16, 16, 'score: 0', {
+
+            fontSize: '32px',
+            fill: '#000'
+        });
+        }
+
+        
     },
+
     update: function() {
-        // Move the player left/right when an arrow key is pressed
-        if (this.left.isDown) {
-            this.player.body.velocity.x = -300;
+
+        game.physics.arcade.collide(this.player, this.platforms);
+
+        this.player.body.velocity.x = 0;
+
+        if (this.cursors.left.isDown) {
+            this.player.body.velocity.x = -150;
+
+            this.player.animations.play('left');
         }
-        else if (this.right.isDown) {
-        this.player.body.velocity.x = 300;
+
+        else if (this.cursors.right.isDown) {
+            this.player.body.velocity.x = 150;
+
+            this.player.animations.play('right');
         }
-        // Stop the player when no key is pressed
+
         else {
-            this.player.body.velocity.x = 0;
+
+            this.player.animations.stop();
+
+            this.player.frame = 4;
         }
-        // Collision between the player and object
-        game.physics.arcade.overlap(this.player, this.objects, this.hitObject, null, this);
+
+        if (this.cursors.up.isDown && this.player.body.touching.down) {
+            this.player.body.velocity.y = -350;
+        }
+
+        game.physics.arcade.collide(this.stars, this.platforms);
+        game.physics.arcade.overlap(this.player, this.stars, this.collectStar, null, this);
 
     },
-    hitObject: function(player, object) {
-        object.kill();
+
+    collectStar: function(player, star) {
+        star.kill();
+        this.score += 5;
+        this.scoreText.text = "Score:" + this.score;
     }
+
 };
+
 game.state.add('main', game_state.main);
-game.state.start('main');
+//game.state.start('main');
---
+++ myGame2/start.js
@@ -0,0 +1,29 @@
+/*global Phaser game  game_state  */
+
+game_state.story = function() {};
+game_state.story.prototype = {
+    preload: function() {
+//      game.load.image('start', 'assets/start.png');
+    },
+    
+    create: function() {
+        game.stage.backgroundColor = '#9182ff';
+//      this.sprite = game.add.sprite(0, 0, 'start');
+        this.enterKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+        
+        this.text = game.add.text(400, 400, "ENTER");
+//      this.text.anchor(0.5);
+        this.text.fill = '#fff';
+        this.text.fontSize = 20;
+    },
+    
+    update: function() {
+        if(this.enterKey.isDown) {
+            console.log("enter pressed")
+            game.state.start('main');
+        }
+    }
+};
+
+game.state.add('story', game_state.story);
+// game.state.start('story');
