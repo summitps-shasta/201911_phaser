be88241bbf1a44a2ae97c167b9a439ad8540f121
EC2 Default User <ec2-user@ip-172-31-45-108.us-east-2.compute.internal>
2020-06-01 02:05:31 +0000

    backup

--- myGame2/myGame2.js
+++ myGame2/myGame2.js
@@ -7,10 +7,26 @@ game_state.main = function() {};
 game_state.main.prototype = {
 
     preload: function() {
-
+        game.load.image('player', 'assets/player.png')
+        game.load.image('object', 'assets/object.png')
     },
 
     create: function() {
+        game.stage.backgroundColor = '#3598db';
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.player = game.add.sprite(200, 400, 'player');
+        game.physics.arcade.enable(this.player);
+        this.player.enableBody = true;
+        this.player.body.immovable = true;
+        this.left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+        this.right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+        this.object = game.add.group();
+        this.object.enableBody = true;
+        var _this = this;
+        setInterval(function() {
+            var object = _this.object.create(Math.random() * 800, -64, 'object');
+            
+        }
 
     },
 
