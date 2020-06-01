3af6ace58d6ebf64bb77efcd017ee6fc180363d1
EC2 Default User <ec2-user@ip-172-31-25-146.us-east-2.compute.internal>
2020-06-01 02:02:34 +0000

    backup

--- myGame2/myGame2.js
+++ myGame2/myGame2.js
@@ -7,17 +7,59 @@ game_state.main = function() {};
 game_state.main.prototype = {
 
     preload: function() {
-
+        game.load.image('player', 'assets/player.png');
+        game.load.image('player', 'assets/object.png');
+alert('test');        
     },
 
     create: function() {
-
+        game.stage.backgroundColor = '#3598db';
+        
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.player = game.add.sprite(200, 400, 'player')
+        game.physics.arcade.enable(this.player);
+        this.player.enableBody = true;
+        alert('test');
+        this.player.body.immovable = true;
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.player = game.add.sprite(200,400, 'player');
+        this.physics.arcade.enable(this.player);
+        this.player.enableBody = true;
+        this.player.body.immovable = true;
+        this.left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT)
+        this.right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT)
+        this.objects.enableBody = true;
+        this.objects = game.add.group();
+        var _this = this;
+        setInterval(function() {
+            var object = _this.objects.create(Math.random() * 800, -64, 'object');
+            object.body.gravity.y = 300;
+        }, 1000) // 1000 = 1000ms = 1 second
+    
     },
 
     update: function() {
-
+        if(this.left.isDown) {
+            this.player.body.velocity. x = -300;
+        }
+        else if (this.right.isDown) {
+            this.player.body.velocity. x = 300;
+        }
+        else{
+            this.player.body.velocity. x = 0;
+        }
+        game.physics.arcade.overlap(this.player, this.objects, this.hitObject, null, this);
     },
 
 }
 game.state.add('main', game_state.main);
 game.state.start('main');
+game.physics.startSystem(Phaser.Physics.ARCADE);
+hitObject: function(player, object){
+    object.kill();
+}
+game.physics.arcade.overlap(this.player, this.objects, this.hitObject, null, this);
+}
+hitObject: function(player, object){
+    object.kill();
+}
