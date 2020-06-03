388fe45c5427cd6a5e4b02f54f96a483c9e2636f
EC2 Default User <ec2-user@ip-172-31-28-6.us-east-2.compute.internal>
2020-06-03 22:48:27 +0000

    backup

--- myGame2/myGame2.js
+++ myGame2/myGame2.js
@@ -7,8 +7,12 @@ game_state.main = function() {};
 game_state.main.prototype = {
 
     preload: function() {
-        game.load.image('player', 'assets/player.png');
+        game.load.spritesheet('player', 'assets/bbball.png', 128, 128);
+        //      game.load.image('player', 'assets/player.png');
         game.load.image('object', 'assets/object.png');
+        game.load.image('sky', 'assets/sky.png');
+        game.load.image('ground', 'assest/platform.png');
+        game.load.image('star', 'assets/star.png');
 
     },
 
@@ -36,8 +40,13 @@ game_state.main.prototype = {
         this.left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
         this.right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
 
+     
         // Create objects group 
         this.objects = game.add.group();
+        
+        // We're going to be using physics, so enable the Arcade Physics system 
+        game.physics.startsSystem(Phaser.Physics.ARCADE);
+        
 
         // Enable body for all objects in the group
         this.objects.enableBody = true;
@@ -53,19 +62,56 @@ game_state.main.prototype = {
             // Let gravity do its thing
             object.body.gravity.y = 300;
         }, 1000) // 1000 = 100ms = 1 second
+
+        // Our two animations walking left and right.
+        this.player.animations.add('left', [2, 3], 10, true);
+        this.player.animations.add('right', [0, 1], 10, true);
+        
+        game.add.sprite(0,0, 'star');
+        
+        // A simple background for game 
+        game.add.sprite(0,0, 'sky');
+        
+        // The platforms group contains the ground and the 2 ledges we can jump on
+        this.platforms=game.add.group();
+        // We will enable physics for any object that is created in this group 
+        this.platforms.enableBody = true;
+        
+        // Here we create the ground.
+        var ground = this.platforms.create(0, game.world.height - 64, 'ground');
+        
+        // Scale it to fit the width of the game (the original sprote is 400x32 in size)
+        ground.scale .setTo(2,2);
+        
+        // This stops it from falling away when yu jump on it 
+        ground.body.immovable = true;
+        
+        // Now let's create two ledges 
+        // var ledge = this.platforms.create(50, 30, 'ground');
+        // ledge.body.immovable = true
+        
+        
+        
+
+      
+
     },
     update: function() {
 
         // Move the player left/right when an arrow key is pressed
         if (this.left.isDown) {
             this.player.body.velocity.x = -300;
+            this.player.animations.play('left');
         }
         else if (this.right.isDown) {
             this.player.body.velocity.x = 300;
+            this.player.animations.play('right');
         }
         // Stop the player when no key is pressed
         else {
             this.player.body.velocity.x = 0;
+            this.player.animations.stop();
+            this.player.frame = 0
         }
 
         // Collision between the player and the object
@@ -75,6 +121,8 @@ game_state.main.prototype = {
     hitObject: function(player, object) {
         object.kill();
     }
+
+    // game.debug.body(this.player);
 };
 game.state.add('main', game_state.main);
 game.state.start('main');
